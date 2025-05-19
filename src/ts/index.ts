233a6905d61085f6276d3f1cc87c895156a7db34
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-05-19 18:13:01 -0300

    feat: Navbar configurado para desktop e mobile

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,14 @@
-window.addEventListener("load", () => {
-  document.getElementById("paragraph")!.textContent = "TEXTO NOVINHO";
+const menu_hamburguer = document.getElementById("menu_hamburguer")!;
+const navbar_items = document.querySelector(".navbar_items")!;
+
+const li_close_menu = document.getElementById("close_menu")!;
+
+menu_hamburguer.addEventListener("click", () => {
+  menu_hamburguer.classList.toggle("hide");
+  navbar_items.classList.toggle("active");
+});
+
+li_close_menu.addEventListener("click", () => {
+  menu_hamburguer.classList.toggle("hide");
+  navbar_items.classList.toggle("active");
 });
