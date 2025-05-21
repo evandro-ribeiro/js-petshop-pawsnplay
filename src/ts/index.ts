e2631e64b9d39acd6a18157f883be2824fc00a77
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-05-21 11:20:07 -0300

    feat: adiciona background e blur na navbar conforme a posição da página

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,4 @@
+// Lógica para abrir e fechar o menu hamburguer da navbar
 const menu_hamburguer = document.getElementById("menu_hamburguer")!;
 const navbar_items = document.querySelector(".navbar_items")!;
 
@@ -11,4 +12,21 @@ menu_hamburguer.addEventListener("click", () => {
 li_close_menu.addEventListener("click", () => {
   menu_hamburguer.classList.toggle("hide");
   navbar_items.classList.toggle("active");
+});
+
+// Colocar backgroud-color e blur na navbar quando faz scroll
+const navbar = document.querySelector(".navbar")!;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add("blur_with_scroll");
+    if (window.scrollY > 700) {
+      navbar.classList.add("background_navbar_with_scroll");
+    } else {
+      navbar.classList.remove("background_navbar_with_scroll");
+    }
+  }
+  if (window.scrollY < 100) {
+    navbar.classList.remove("blur_with_scroll");
+  }
 });
