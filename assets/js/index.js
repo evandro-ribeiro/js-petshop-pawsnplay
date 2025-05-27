b1ed8df635c1682546f02d66eba9f67a239b0503
evandro-ribeiro <evandrormf.dev@gmail.com>
2025-05-27 16:35:40 -0300

    feat: adiciona input de pesquisa da seção shop na versão mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,19 @@ window.addEventListener("scroll", () => {
         ? navbar.classList.add("background_navbar_with_scroll")
         : navbar.classList.remove("background_navbar_with_scroll");
 });
+// Coloca efeito de clique nos cards de produtos
+document.querySelectorAll(".product_hover").forEach((card) => {
+    card.addEventListener("click", function () {
+        document.querySelectorAll(".product_hover").forEach((el) => {
+            if (el !== card)
+                el.classList.remove("active");
+        });
+        card.classList.toggle("active");
+    });
+});
+// Abre caixa de pesquisa da seção shop
+const searchIcon = document.getElementById("search_icon");
+searchIcon.addEventListener("click", () => {
+    const inputSearch = document.querySelector(".input_search");
+    inputSearch.classList.toggle("hide_mobile");
+});
